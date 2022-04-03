af1bfddfcf1c810511dc70e5e142c58945e4cd6f
BuildTools <unconfigured@null.spigotmc.org>
2022-04-03 21:16:32 +0200

    Saving

--- dinosaur.php
+++ dinosaur.php
@@ -1,14 +1,30 @@
 <?php
 
-require_once 'Util.php';
+$array = [
+    "NAME" => [],
+    "LEG_LENGTH" => [],
+    "DIET" => [],
+    "STRIDE_LENGTH" => [],
+    "STANCE" => [],
+];
 
-$d1 = Util::getFileText("dataset1.csv");
-echo $d1;
+$d1 = array_map("str_getcsv", file("dataset1.csv"));
+array_splice($d1, 0, 1);
+print_r($d1);
 
-$legs = str_split($d1, ", ");
+$d2 = array_map("str_getcsv", file("dataset2.csv"));
+array_splice($d2, 0, 1);
+print_r($d2);
 
-foreach($legs as $l) {
-    echo $l;
+for($i = 0, $iMax = count($array); $i < $iMax; $i++) {
+    $array["LEG_LENGTH"][] = $d1["LEG_LENGTH"];
+    $array["DIET"][] = $d1["DIET"];
+
+    if($d1["NAME"] === $d2["NAME"]) {
+        $array["NAME"] = $d2["NAME"];
+        $array["STRIDE_LENGTH"] = $d2["STRIDE_LENGTH"];
+        $array["STANCE"] = $d2["STANCE"];
+    }
 }
 
-echo "";
+print_r($array);
