25a6f95a10ddc5035c9dff2afaaf0cd7dfc23aed
RusterX16 <EliottRuster@gmail.com>
2022-04-04 09:53:27 +0200

    Arrays merge

--- dinosaur.php
+++ dinosaur.php
@@ -1,29 +1,23 @@
 <?php
 
-$array = [
-    "NAME" => [],
-    "LEG_LENGTH" => [],
-    "DIET" => [],
-    "STRIDE_LENGTH" => [],
-    "STANCE" => [],
-];
+$array = [];
 
 $d1 = array_map("str_getcsv", file("dataset1.csv"));
 array_splice($d1, 0, 1);
-print_r($d1);
 
 $d2 = array_map("str_getcsv", file("dataset2.csv"));
 array_splice($d2, 0, 1);
-print_r($d2);
 
-for($i = 0, $iMax = count($array); $i < $iMax; $i++) {
-    $array["LEG_LENGTH"][] = $d1["LEG_LENGTH"];
-    $array["DIET"][] = $d1["DIET"];
+for($j = 0, $jMax = count(max($d1, $d2)); $j < $jMax; $j++) {
+    $array[] = $d1[$j];
+}
 
-    if($d1["NAME"] === $d2["NAME"]) {
-        $array["NAME"] = $d2["NAME"];
-        $array["STRIDE_LENGTH"] = $d2["STRIDE_LENGTH"];
-        $array["STANCE"] = $d2["STANCE"];
+for($i = 0, $iMax = count(max($d1, $d2)); $i < $jMax; $i++) {
+    for($j = 0, $jMax = count(max($d1, $d2)); $j < $jMax; $j++) {
+        if($d2[$i][0] === $d1[$j][0]) {
+            $array[$i][] = $d2[$j][1];
+            $array[$i][] = $d2[$j][2];
+        }
     }
 }
 
